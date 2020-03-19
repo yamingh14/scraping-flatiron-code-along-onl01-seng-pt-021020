4bde352a49b5823b0978eb64217ce78e82e45ee0
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 05:18:09 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,105 +1,36 @@
+require 'nokogiri'
 require 'open-uri'
-require 'pry'
-require 'nokogiri' #=> I added this one
-require 'rubygems' #=> I added this one
+
+require_relative './course.rb'
+
 class Scraper
 
-  def self.scrape_index_page(index_url)
-    html = open(index_url) #=> index_url = "../fixtures/student-site/index.html"
-    list = Nokogiri::HTML(html)
-
-    # This block returns a list of student names
-    names = list.css(".student-name")
-    names_array = []
-    names.each do |item|
-      names_array << item.text
-    end
-    names_array
-
-    # This block returns a list of locations.
-    locations = list.css(".student-location")
-    location_array = []
-    locations.each do |item|
-      location_array << item.text
-    end
-    location_array
-
-    # This block returns a list of student HTML pages.
-    webpages = list.css(".student-card a[href]")
-    webpage_array = []
-    webpages.select do |item|
-      webpage_array << item['href']
-    end
-    webpage_array
-
-
-    master_array = []
-    hash = {}
-    x = 0
-    names_array.each do |name|
-      master_array << {:name => name, :location => location_array[x], :profile_url => webpage_array[x]}
-      x = x + 1
-    end
-    master_array
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
-  def self.scrape_profile_page(profile_url)
-    html = open(profile_url)
-    doc = Nokogiri::HTML(html)
-
-    students_twitter = []
-    students_linkedin = []
-    students_github = []
-    students_youtube = []
-    students_blog = []
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if link['href'].include?("twitter")
-        students_twitter << link['href']
-      end
-    end
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if link['href'].include?("linkedin")
-        students_linkedin << link['href']
-      end
-    end
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if link['href'].include?("github")
-        students_github << link['href']
-      end
-    end
-
-    doc.xpath('//div[@class="social-icon-container"]/a').map do |link|
-      if !(link['href'].include?("github")) &&
-         !(link['href'].include?("linkedin")) &&
-         !(link['href'].include?("twitter"))
-         students_blog = link['href']
-      end
-    end
-
-    students_quote = doc.css(".profile-quote").text
-    students_bio = doc.css(".description-holder p").text
-
-    students_twitter
-    students_linkedin
-    students_github
-    students_youtube
-      if students_youtube = ""
-        students_youtube = nil
-      end
-    students_blog
-
-    # This needs to be an iteration where it starts with <<output = {}>>
-    out_put = {:twitter=>students_twitter[0],
-               :linkedin=>students_linkedin[0],
-               :github=>students_github[0],
-               :profile_quote=>students_quote,
-               :blog=>students_blog,
-               :bio=> students_bio}
-    out_put.delete_if {|key, value| value == [] || value == nil}
-
-    out_put
+  def get_courses
+    self.get_page.css(".post")
   end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
